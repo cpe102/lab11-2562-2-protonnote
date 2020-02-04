603c0491e20bfa29a2866e53b1933c593166f2c5
lab314-20@labeng.local <lab314-20@labeng.local>
2020-02-04 16:07:03 +0700

    NUMBA 1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -16,8 +16,7 @@ int main(){
 	return 0;
 }
 unsigned long long int fibonacci(int a) {
-		if (a == 0 && a == 1) return 0;
-		if (a>1){
-			return  fibonacci(a-1)+fibonacci(a-2);
-		}
+		if (a == 0) return 0;
+		else if (a == 1) return 1;
+		if (a>1) return  fibonacci(a-1)+fibonacci(a-2);
 }
